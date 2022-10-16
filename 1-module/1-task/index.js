aec2c07b002f08f993913ccbe3512b60203f7253
BorisNovi <borisnovikov4815@gmail.com>
2022-10-17 00:42:57 +0400

    I called it fucktorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,20 @@
 function factorial(n) {
-  // ваш код...
-}
+
+  let m = n;
+
+if (n != 0){
+  
+  while (n > 1){
+
+    m = m * (n-1)
+
+    n -= 1
+
+  }; 
+  
+return m
+};
+
+return m = 1
+
+};
