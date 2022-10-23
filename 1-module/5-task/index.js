52a0e12197e33e137aa3a690222562bd07660462
BorisNovi <borisnovikov4815@gmail.com>
2022-10-23 18:48:19 +0400

    Ok, it's working.

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,11 @@
 function truncate(str, maxlength) {
-  // ваш код...
+
+if (str.length <= maxlength) {
+  return str
+};
+
+if (str.length > maxlength) {
+  return str.slice(0, maxlength - 1) + '…'
+};
+
 }
