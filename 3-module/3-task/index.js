16f76c5787f2f73bbc3e7e4dec51140288ebeb9d
BorisNovi <borisnovikov4815@gmail.com>
2022-10-30 20:59:44 +0400

    Looks like human centipede

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
-}
+  cam = str.split("-").map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1))
+  .join('')
+  //console.log(cam);
+  return cam;
+};
+
+// camelize('background-color') //== 'backgroundColor';
+// camelize('list-style-image') //== 'listStyleImage';
+// camelize('-webkit-transition') //== 'WebkitTransition';
