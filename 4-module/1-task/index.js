84ae6727f28e91acbf9b7d5c3c2108b1f7174fd1
BorisNovi <borisnovikov4815@gmail.com>
2022-11-08 01:12:53 +0400

    Ok

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let ul = document.createElement('ul');
+  document.body.append(ul);
+  
+  for (i in friends){
+    let plus = friends[i].firstName + " " + friends[i].lastName;
+    let li = document.createElement('li');
+    li.innerHTML = plus;
+    ul.append(li);
+  };
+  
+  return ul;
+};
