e629f7ad44c725a43db26a6c2a4b34cced78ce5b
BorisNovi <borisnovikov4815@gmail.com>
2022-12-06 19:34:51 +0400

    Corrected

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,8 +1,18 @@
+// function toggleText() {
+//   document.addEventListener('click', function(event){
+//     let btn = event.target.className;
+//     if (!btn) return;
+//     let elem = document.getElementById("text");
+//     elem.hidden = !elem.hidden;
+//   });
+// }
+
 function toggleText() {
-  document.addEventListener('click', function(event){
-    let btn = event.target.className;
-    if (!btn) return;
-    let elem = document.getElementById("text");
-    elem.hidden = !elem.hidden;
+  let toggleButton = document.querySelector('.toggle-text-button');
+  let textElement = document.getElementById('text');
+
+  toggleButton.addEventListener('click', () => {
+    let isTextElementHidden = textElement.hidden;
+    textElement.hidden = !isTextElementHidden;
   });
 }
